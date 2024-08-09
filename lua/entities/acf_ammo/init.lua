99efc3f842985d17047cbcb5c7fb5c1251f70dc4
Mr Marty <57878680+MartyX5555@users.noreply.github.com>
2024-08-08 22:19:55 -0400

    added capacity output to ammo crates

--- lua/entities/acf_ammo/init.lua
+++ lua/entities/acf_ammo/init.lua
@@ -10,45 +10,53 @@ local GunTable  = ACF.Weapons.Guns
 local AmmoTable = ACF.Weapons.Ammo
 local LegacyAmmoTable = ACF.Weapons.LegacyAmmo
 
+local Inputs = {
+	"Active"
+}
+local Outputs = {
+	"Munitions (Returns the current number of rounds in this crate.)",
+	"Capacity (Returns the capacity of this crate.)"
+}
+
 function ENT:Initialize()
 
-	self.SpecialHealth	= true  --If true needs a special ACF_Activate function
-	self.SpecialDamage	= true  --If true needs a special ACF_OnDamage function
+	self.SpecialHealth       = true  --If true needs a special ACF_Activate function
+	self.SpecialDamage       = true  --If true needs a special ACF_OnDamage function
 
-	self.IsExplosive		= true
-	self.Exploding		= false
-	self.Damaged			= false
+	self.IsExplosive         = true
+	self.Exploding           = false
+	self.Damaged             = false
 
-	self.CanUpdate		= true
-	self.Load			= false
-	self.EmptyMass		= 1
-	self.AmmoMassMax		= 0
-	self.NextMassUpdate	= 0
+	self.CanUpdate           = true
+	self.Load                = false
+	self.EmptyMass           = 1
+	self.AmmoMassMax         = 0
+	self.NextMassUpdate      = 0
 
-	self.Ammo			= 0
-	self.IsTwoPiece		= false
+	self.Ammo                = 0
+	self.IsTwoPiece          = false
 
-	self.NextLegalCheck	= ACF.CurTime + math.random(ACF.Legal.Min, ACF.Legal.Max) -- give any spawning issues time to iron themselves out
-	self.Legal			= true
-	self.LegalIssues		= ""
+	self.NextLegalCheck      = ACF.CurTime + math.random(ACF.Legal.Min, ACF.Legal.Max) -- give any spawning issues time to iron themselves out
+	self.Legal               = true
+	self.LegalIssues         = ""
 
-	self.Active			= false
-	self.Master			= {}
-	self.Sequence		= 0
+	self.Active              = false
+	self.Master              = {}
+	self.Sequence            = 0
 
-	self.Interval		= 1		-- Think Interval when its not damaged
-	self.ExplosionInterval  = 0.01	-- Base Think Interval when its damaged and its about to explode
+	self.Interval            = 1		-- Think Interval when its not damaged
+	self.ExplosionInterval   = 0.01	-- Base Think Interval when its damaged and its about to explode
 
-	self.Capacity		= 1
-	self.AmmoMassMax		= 1
-	self.Caliber			= 1
-	self.RoFMul			= 1
-	self.LastMass		= 1
+	self.Capacity            = 1
+	self.AmmoMassMax         = 1
+	self.Caliber             = 1
+	self.RoFMul              = 1
+	self.LastMass            = 1
 
-	self.Inputs			= Wire_CreateInputs( self, { "Active" } ) --, "Fuse Length"
-	self.Outputs			= Wire_CreateOutputs( self, { "Munitions" } )
+	self.Inputs              = Wire_CreateInputs( self, Inputs )
+	self.Outputs             = Wire_CreateOutputs( self, Outputs )
 
-	ACF.AmmoCrates		= ACF.AmmoCrates or {}
+	ACF.AmmoCrates           = ACF.AmmoCrates or {}
 
 end
 
@@ -597,6 +605,7 @@ do
 		self.NetworkData = ACF.RoundTypes[self.BulletData.Type].network
 		self:NetworkData( self.BulletData )
 
+		Wire_TriggerOutput( self, "Capacity", self.Capacity )
 		self:UpdateOverlayText()
 
 	end
