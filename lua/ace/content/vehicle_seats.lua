6d62864181dbc6c68c4b23a814626753fab84098
Mr Marty <57878680+MartyX5555@users.noreply.github.com>
2026-06-02 16:40:21 -0400

    version update

--- lua/ace/content/vehicle_seats.lua
+++ lua/ace/content/vehicle_seats.lua
@@ -2,45 +2,42 @@
 local Category = "Armoured Combat Framework" -- If ACE3 or ACE, the vehicles should be in the same category
 
 -- There is ACE or ACE3 installed
-if not ACE then
 
-	local function HandleACEPodAnimation( _, player )
-		return player:LookupSequence("drive_pd")
-	end
-
-	list.Set( "Vehicles", "acf_pod", {
-		Name = "Standard Driver Pod",
-		Class = "prop_vehicle_prisoner_pod",
-		Category = Category,
-
-		Author = "Lazermaniac",
-		Information = "Modified prisonpod for more realistic player damage",
-		Model = "models/vehicles/driver_pod.mdl",
-		KeyValues = {
-			vehiclescript	=	"scripts/vehicles/prisoner_pod.txt",
-			limitview		=	"0"
-		},
-		Members = {
-			HandleAnimation = HandleACEPodAnimation
-		}
-	} )
-
-	list.Set( "Vehicles", "acf_pilotseat", {
-
-		Name = "Standard Pilot Seat",
-		Class = "prop_vehicle_prisoner_pod",
-		Category = Category,
-
-		Author = "Lazermaniac",
-		Information = "A generic seat for accurate damage modelling.",
-		Model = "models/vehicles/pilot_seat.mdl",
-		KeyValues = {
-			vehiclescript	=	"scripts/vehicles/prisoner_pod.txt",
-			limitview		=	"0"
-		},
-	} )
-
+local function HandleACEPodAnimation( _, player )
+	return player:LookupSequence("drive_pd")
 end
+
+list.Set( "Vehicles", "acf_pod", {
+	Name = "Standard Driver Pod",
+	Class = "prop_vehicle_prisoner_pod",
+	Category = Category,
+
+	Author = "Lazermaniac",
+	Information = "Modified prisonpod for more realistic player damage",
+	Model = "models/vehicles/driver_pod.mdl",
+	KeyValues = {
+		vehiclescript	=	"scripts/vehicles/prisoner_pod.txt",
+		limitview		=	"0"
+	},
+	Members = {
+		HandleAnimation = HandleACEPodAnimation
+	}
+} )
+
+list.Set( "Vehicles", "acf_pilotseat", {
+
+	Name = "Standard Pilot Seat",
+	Class = "prop_vehicle_prisoner_pod",
+	Category = Category,
+
+	Author = "Lazermaniac",
+	Information = "A generic seat for accurate damage modelling.",
+	Model = "models/vehicles/pilot_seat.mdl",
+	KeyValues = {
+		vehiclescript	=	"scripts/vehicles/prisoner_pod.txt",
+		limitview		=	"0"
+	},
+} )
 
 
 
